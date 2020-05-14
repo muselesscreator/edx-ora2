c4d25dec71b8c4b7a59c4d125068817c8ee69dfd
Ben Warzeski <benwarzeski@edX-C02CD0HCLVDM.local>
2020-05-14 13:37:30 -0400

    build config attempts

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,17 +1,20 @@
 const { getBaseConfig } = require('@edx/frontend-build');
 const config = getBaseConfig('webpack');
-
+const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path');
 
 Object.assign(config, {
   entry: {
-    server: path.resolve(process.cwd(), 'openassessment/xblock/static/js/src/oa_server.js'),
-    shared: path.resolve(process.cwd(), 'openassessment/xblock/static/js/src/oa_server.js'),
+    studio: path.resolve(process.cwd(), 'openassessment/xblock/static/js/src/studio.js'),
+    lms: path.resolve(process.cwd(), 'openassessment/xblock/static/js/src/lms.js'),
   },
   output: {
     filename: '[name].[chunkhash].js',
-    path: path.resolve(process.cwd(), 'openassessment/xblock/static/js'),
+    path: path.resolve(process.cwd(), 'openassessment/xblock/static/js/dist'),
   },
+  optimization: {
+    minimizer: [ new TerserPlugin() ],
+  }
 });
 
 config.plugins.splice(4, 1);
